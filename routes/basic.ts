1ffd17d5278fe1b79c9c90bf2ccf221a085ee89b
zeroyukiy <zeroyukiy@hotmail.com>
2022-07-01 19:42:59 +0200

    added image

--- routes/basic.ts
+++ routes/basic.ts
@@ -1,15 +1,34 @@
 import express, { Request, Response, Router } from 'express';
+import stream from 'stream';
+import fs from 'fs';
 
 const router: Router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-  console.log(req.headers.referer);
+  // console.log(req.headers.referer);
 
   res.json({
     status: 'Ok',
     statusCode: 200,
     data: {},
   });
+});
+
+router.get('/image', (req: Request, res: Response) => {
+  const r: string | undefined = req.headers.referer;
+  fs.writeFile('log.txt', r ? r : '', (err) => {
+    if (err) console.error(err);
+  });
+
+  const readStream = fs.createReadStream('./images/100x100.png');
+  const ps = new stream.PassThrough();
+  stream.pipeline(readStream, ps, (err) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(400);
+    }
+  });
+  ps.pipe(res);
 });
 
 export default router;
