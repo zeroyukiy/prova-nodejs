6c9cbfd0609f5ff71b4cd721cb8f459bfa958fc0
zeroyukiy <zeroyukiy@hotmail.com>
2022-07-01 19:44:05 +0200

    build js

--- build/routes/basic.js
+++ build/routes/basic.js
@@ -4,13 +4,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const stream_1 = __importDefault(require("stream"));
+const fs_1 = __importDefault(require("fs"));
 const router = express_1.default.Router();
 router.get('/', (req, res) => {
-    console.log(req.headers.referer);
+    // console.log(req.headers.referer);
     res.json({
         status: 'Ok',
         statusCode: 200,
         data: {},
     });
+});
+router.get('/image', (req, res) => {
+    const r = req.headers.referer;
+    fs_1.default.writeFile('log.txt', r ? r : '', (err) => {
+        if (err)
+            console.error(err);
+    });
+    const readStream = fs_1.default.createReadStream('./images/100x100.png');
+    const ps = new stream_1.default.PassThrough();
+    stream_1.default.pipeline(readStream, ps, (err) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+    });
+    ps.pipe(res);
 });
 exports.default = router;
